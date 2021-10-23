ef2d3259e6bb51e61a61694e1a18248610407f27
surajadkhari <surajadkhari@gmail.com>
2021-10-23 16:09:29 +0545

    Check branch

---
+++ android/app/src/main/kotlin/com/example/parse_json_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.parse_json_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/user_model.dart
@@ -0,0 +1,13 @@
+class UsersModel {
+  final int id;
+  final String email;
+  final String first_name;
+  final String last_name;
+  final String avatar;
+  UsersModel(
+      {required this.id,
+      required this.email,
+      required this.first_name,
+      required this.last_name,
+      required this.avatar});
+}
