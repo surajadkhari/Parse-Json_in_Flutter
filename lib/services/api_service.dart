6f19f7c59c314d464503518d350fcb57532b41b7
surajadkhari <surajadkhari@gmail.com>
2021-10-23 16:26:28 +0545

    Commplete Mapping data in service

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -8,5 +8,19 @@ class Apiservice {
   Future<dynamic> fetchData() async {
     Response response = await get(Uri.parse(endPoint));
     List jsonData = jsonDecode(response.body)['data'];
+
+//Mapping
+    List userData = [];
+    for (int index = 0; index < jsonData.length; index++) {
+      userData.add(
+        UsersModel(
+            id: jsonData[index]['id'],
+            email: jsonData[index]['email'],
+            first_name: jsonData[index]['first_name'],
+            last_name: jsonData[index]['last_name'],
+            avatar: jsonData[index]['avatar']),
+      );
+    }
+    return userData;
   }
 }
